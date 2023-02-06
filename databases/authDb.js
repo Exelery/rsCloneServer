ad923dcb70b222d05a1b219bd645b1ecf64a45be
Owner <test>
2023-02-06 18:04:10 +0300

    fix: columns

--- databases/authDb.js
+++ databases/authDb.js
@@ -21,7 +21,7 @@ authDb.connect((err) => {
 
 const createTable =`CREATE TABLE IF NOT EXISTS ${process.env.TABLENAME} (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(200) NOT NULL UNIQUE,
+  email VARCHAR(200) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );`
