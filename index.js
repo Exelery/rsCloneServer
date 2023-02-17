a7e88ff0979b50f1146e903cb0289ac5d95c23a8
Owner <test>
2023-02-18 00:19:23 +0300

    feat:  improve cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const start = async () => {
     const PORT = process.env.PORT
     const app = express()
     // await Connection.getInstance()
-    app.use(cors()); // {credentials: true,origin: process.env.CLIENT_URL}
+    app.use(cors({credentials: true,origin: process.env.SITE_URL})); // 
     app.use(express.json());
     app.use(cookieParser());
     
---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export default class UserModel {
+
+  constructor(connection) {
+    this.bd = connection
+    this.dataPath = process.cwd() + '/data'
+    mkdir(`${this.dataPath}`, { recursive: true })
+  }
+
+  async createUser(name, email, password) {
+    const sql = `INSERT INTO ${process.env.TABLENAME}(name, email, password, activationLink) VALUES("${name}", "${email}", "${passwordHash}", "${activationLink}")`;
+    const answer = await this.bd.query(sql)
+    return answer[0].insertId
+  }
+
+  async getUserByEmail(email) {
+    const sqlCheck = `SELECT id, email, password FROM ${process.env.TABLENAME} WHERE email = "${email}"`;
+    const answer = await this.bd.query(sqlCheck)
+    return answer[0]
+  }
+
+
+
+}
